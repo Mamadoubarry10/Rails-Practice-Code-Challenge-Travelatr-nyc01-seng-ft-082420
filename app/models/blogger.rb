7bd4693f7b8b29a40ec9b282f0def4fc169cc479
Mamadoubarry10 <Mamadou.barry1105@gmail.com>
2020-09-21 22:02:29 -0400

    finally done

--- app/models/blogger.rb
+++ app/models/blogger.rb
@@ -6,5 +6,18 @@ class Blogger < ApplicationRecord
     validates :age, numericality: { greater_than: 0 }
     validates :bio, length: { minimum: 30 } 
 
+   
+
+    def most_liked
+        max_likes = self.posts.map { |post| post.likes }.max
+        self.posts.find_all { |post| post.likes == max_likes }
+    end
+
+    def average_age
+        self.all.collect { |blogger| blogger.age }.sum / self.all.length
+    end
+
+
+
 
 end
--- app/models/destination.rb
+++ app/models/destination.rb
@@ -1,4 +1,26 @@
 class Destination < ApplicationRecord
     has_many :posts
     has_many :bloggers, through: :posts
+
+
+
+
+    def most_five_recent
+        self.posts.take(5).each do |post|
+            post.content
+        end
+    end
+    def most_liked
+        max_likes = self.posts.map { |post| post.likes }.max
+        self.posts.find_all { |post| post.likes == max_likes }
+    end
+     
+    def avg_age 
+        self.bloggers.distinct.average(:age).round()
+    end
+        
+        
 end
+
+
+
